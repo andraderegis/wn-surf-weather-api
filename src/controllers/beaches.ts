9a71066ec990c6f64350234d39d39215323570ea
andraderegis <reginaldo.andrade@outlook.com>
2020-11-10 18:00:26 -0300

    Adicionada a model de Beach. Teste para campo invalido ao cadastrar uma beach

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,21 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import { Beach } from '@src/models/beach';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    res.status(201).send({ id: 'id', ...req.body });
+    try {
+      const beach = new Beach(req.body);
+      const result = await beach.save();
+      res.status(201).send(result);
+    } catch (e) {
+      e instanceof mongoose.Error.ValidationError
+        ? res.status(422).send({ error: e.message })
+        : res.status(500).send({ error: 'Internal Server Error' });
+    }
+    // res.status(201).send({ id: 'id', ...req.body });
   }
 }
--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,7 @@
+import { Beach } from '@src/models/beach';
+
 describe('Beaches functional tests', () => {
+  beforeAll(async () => await Beach.deleteMany({}));
   describe('When creating a beach', () => {
     it('Should create a beach with success', async () => {
       const newBeach = {
@@ -12,6 +15,23 @@ describe('Beaches functional tests', () => {
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+
+    it('Should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_number',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E'
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_number" at path "lat"'
+      });
     });
   });
 });
